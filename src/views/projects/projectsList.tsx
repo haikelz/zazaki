3cf3b7b65a96ad13f0ec160629c4db1def85a571
haikelz <haikel.2017th@gmail.com>
2022-06-02 06:37:30 +0700

    Update

--- src/views/projects/projectsList.tsx
+++ src/views/projects/projectsList.tsx
@@ -1,28 +1,37 @@
-import { createEffect, createSignal } from "solid-js";
-import Gambar from "./gambar";
+import { FiGithub, FiLink } from "solid-icons/fi";
 
 const ProjectsList = () => {
-  // example fetch API in solidJS
-  const [gambar, setGambar] = createSignal([]);
-
-  let getGambar = async () => {
-    try {
-      let response = await fetch(
-        "https://picsum.photos/v2/list?page=1&limit=9"
-      ).then((res) => res.json());
-
-      setGambar(response);
-      console.log(response);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  createEffect(() => {
-    getGambar();
-  });
-
-  return <Gambar gambar={gambar()} />;
+  const images = [
+    { ket: "Project 1" },
+    { ket: "Project 2" },
+    { ket: "Project 3" },
+    { ket: "Project 4" },
+    { ket: "Project 5" },
+    { ket: "Project 6" },
+  ];
+  return (
+    <>
+      {images.map((image: any, index: number) => (
+        <div class="rounded-lg overflow-hidden bg-slate-200 dark:bg-gray-800 border-[0.5px] border-gray-500">
+          <img src="/src/assets/Project.jpg" width="600px" height="400px" />
+          <div class="p-2">
+            <h1 class="text-lg font-semibold">Project {index + 1}</h1>
+            <p class="mt-1 text-base">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi.
+            </p>
+            <div class="mt-2 exclude flex justify-end items-center">
+              <a href="https://github.com">
+                <FiGithub class="cursor-pointer my-2 mr-1 ml-2 transition duration-300 hover:text-red-500" />
+              </a>
+              <a href="https://github.com">
+                <FiLink class="cursor-pointer my-2 ml-1 mr-2 transition duration-300 hover:text-red-500" />
+              </a>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default ProjectsList;
